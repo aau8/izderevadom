a784eaa78cbf332dab969e029657cd1e594770a7
Artem <artiom.ygryumov@gmail.com>
2022-07-12 20:46:56 +0300

    часть слайдера раздела "Доверие"

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,7 +1,7 @@
-import Swiper, { Navigation, Pagination } from "swiper"
+import Swiper, { Navigation, Pagination, EffectFade } from "swiper"
 
 const reviewsSwiper = new Swiper(".reviews__slider", {
-    modules: [ Navigation, Pagination ],
+    modules: [ Navigation ],
 
 
 	breakpoints: {
@@ -44,7 +44,7 @@ const reviewsSwiper = new Swiper(".reviews__slider", {
 });
 
 const gallerySwiper = new Swiper(".gallery__slider", {
-    modules: [ Navigation, Pagination ],
+    modules: [ Navigation ],
 
 	breakpoints: {
 		950: {
@@ -82,5 +82,49 @@ const gallerySwiper = new Swiper(".gallery__slider", {
     navigation: {
         nextEl: ".gallery__arrow-next",
         prevEl: ".gallery__arrow-prev",
+    },
+});
+
+const trustSwiper = new Swiper(".trust-slider__slider", {
+    modules: [ Navigation, Pagination, EffectFade ],
+
+	effect: 'fade',
+
+	breakpoints: {
+		// 950: {
+		// 	slidesPerView: 5,
+		// 	spaceBetween: 20,
+		// 	centeredSlides: false,
+		// 	loop: true,
+		// },
+		// 768: {
+		// 	slidesPerView: 4,
+		// 	centeredSlides: true,
+		// 	spaceBetween: 20,
+		// 	loop: true,
+		// },
+		// 600: {
+		// 	slidesPerView: 2,
+		// 	centeredSlides: true,
+		// 	spaceBetween: 20,
+		// 	loop: true,
+		// },
+		// 450: {
+		// 	slidesPerView: 1.6,
+		// 	centeredSlides: true,
+		// 	spaceBetween: 20,
+		// 	loop: true,
+		// },
+		// 0: {
+		// 	slidesPerView: 1.2,
+		// 	centeredSlides: true,
+		// 	spaceBetween: 10,
+		// 	loop: true,
+		// },
+	},
+
+    navigation: {
+        nextEl: ".trust-slider__arrow-next",
+        prevEl: ".trust-slider__arrow-prev",
     },
 });
